147a6a48b7d363d2c63b5e70b516e53ab857ef34
prashantn/12 <prashantnangare0872@gmail.com>
2024-10-15 12:02:03 +0530

    Added: Welcome in cab invoice Generator problem

---
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.cabinvoicegenerator;
+
+public class CabInvoiceGenerator {
+    public static void main(String[] args) {
+        System.out.println("Welcome in cab invoice generator problem");
+    }
+}
