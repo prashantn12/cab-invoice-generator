9b525909eda62c208d308ba16aee401320777b7f
prashantn/12 <prashantnangare0872@gmail.com>
2024-10-15 12:06:51 +0530

    Added: Calculate Fare

--- src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
+++ src/main/java/com/bridgelabz/cabinvoicegenerator/CabInvoiceGenerator.java
@@ -1,7 +1,21 @@
 package com.bridgelabz.cabinvoicegenerator;
 
 public class CabInvoiceGenerator {
+    private static final double COST_PER_KM = 10.0;
+    private static final double COST_PER_MINUTE = 1.0;
+    private static final double MINIMUM_FARE = 5.0;
+
+    public double calculateFare(double distance, double time) {
+
+        double totalFare = (distance * COST_PER_KM) + (time * COST_PER_MINUTE);
+
+        return Math.max(totalFare, MINIMUM_FARE);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Welcome in cab invoice generator problem");
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 3.0;
+        double time = 15.0;
+        System.out.println(invoiceGenerator.calculateFare(distance,time));
     }
 }
--- src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
+++ src/test/java/com/bridgelabz/cabinvoicegenerator/InvoiceGeneratorTest.java
@@ -1,4 +1,19 @@
 package com.bridgelabz.cabinvoicegenerator;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class InvoiceGeneratorTest {
+    @Test
+    public void testCalculateFare() {
+        //Arrange
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        //Act
+        double distance1 = 3;
+        double time1 = 15;
+        double expectedFare = (3 * 10) + (15 * 1);
+        //Assert
+        Assertions.assertEquals(expectedFare, cabInvoiceGenerator.calculateFare(distance1, time1));
+    }
+
 }
